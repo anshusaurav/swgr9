e619ceac11038907ac1f2660be7bf2393e80a1e4
anshusaurav <anshu.saurav@gmail.com>
2020-09-23 02:31:22 +0530

    Config Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,11 +14,40 @@ module.exports = {
         },
         rotate: {
           "100%": { transform: "rotate(360deg)" }
+        },
+        dash: {
+          "0%": {
+            strokeDasharray: "1, 200",
+            strokeDashoffset: 0
+          },
+          "50%": {
+            strokeDasharray: "89, 200",
+            strokeDashoffset: 0
+          },
+          "100%": {
+            strokeDasharray: "89, 200",
+            strokeDashoffset: 124
+          }
+        },
+        colorwhite: {
+          "100%, 0%": {
+            stroke: "#fff"
+          },
+          "40%": {
+            stroke: "#fff"
+          },
+          "66%": {
+            stroke: "#fff"
+          },
+          "80%, 90%": {
+            stroke: "#fff"
+          },
         }
       },
       animation: {
         wiggle: "wiggle 1s ease-in-out infinite",
-        rotate: "rotate 2s linear infinite"
+        rotate: "rotate 2s linear infinite",
+        dash: "dash 1.5s ease-in-out infinite, colorwhite 6s ease-in-out infinite"
       }
     }
   },
